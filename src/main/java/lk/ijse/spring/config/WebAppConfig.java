28d14b6433fd97657c3916e87b98bb3a380645e4
jayani018 <jayaniarunika11@gmail.com>
2023-10-15 23:13:09 +0530

    Updated WebAppConfig class

--- src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,12 +1,15 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author : Jayani_Arunika  10/15/2023 - 5:53 PM
  * @since : v0.01.0
  **/
 @Configuration
+@ComponentScan(basePackages = "lk.ijse.spring.controller")
 public class WebAppConfig {
     public WebAppConfig(){
         System.out.println("WebAppConfig : Instantiated");
