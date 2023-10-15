166d2c753f79b2c15d084f256b146e2ba5c9614d
jayani018 <jayaniarunika11@gmail.com>
2023-10-15 19:19:45 +0530

    Created Config Class

--- src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,9 +1,29 @@
 package lk.ijse.spring;
 
+import lk.ijse.spring.config.WebAppConfig;
+import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import java.util.WeakHashMap;
+
 /**
  * @author : Jayani_Arunika  10/15/2023 - 4:04 PM
  * @since : v0.01.0
  **/
 
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
---
+++ src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Jayani_Arunika  10/15/2023 - 5:52 PM
+ * @since : v0.01.0
+ **/
+@Configuration
+public class JPAConfig {
+    public JPAConfig(){
+        System.out.println("JPAConfig : Instantiated");
+    }
+}
