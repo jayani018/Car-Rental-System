759a56d35bdfdb64b1cada3aeaf1ea97828c05c5
jayani018 <jayaniarunika11@gmail.com>
2023-10-29 16:34:51 +0530

    Updated DriverServiceImpl

--- car_Rent_Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
+++ car_Rent_Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -1,7 +1,13 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.entity.Driver;
+import lk.ijse.spring.repo.AdminRepo;
+import lk.ijse.spring.repo.DriverRepo;
 import lk.ijse.spring.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,23 +20,43 @@ import java.util.ArrayList;
 @Service
 @Transactional
 public class DriverServiceImpl implements DriverService {
+
+    @Autowired
+    private DriverRepo repo;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public void saveDriver(DriverDTO dto) {
-
+        if (repo.existsById(dto.getDriverId())) {
+            throw new RuntimeException("Driver Already Exist. Please enter another id..!");
+        }
+        repo.save(mapper.map(dto, Driver.class));
     }
 
     @Override
     public void updateDriver(DriverDTO dto) {
+        if (!repo.existsById(dto.getDriverId())) {
+            throw new RuntimeException("Driver Not Exist. Please enter Valid id..!");
+        }
+        repo.save(mapper.map(dto, Driver.class));
 
     }
 
     @Override
     public void deleteDriver(String id) {
+        if (!repo.existsById(id)) {
+            throw new RuntimeException("Wrong ID..Please enter valid id..!");
+        }
+        repo.deleteById(id);
 
     }
 
     @Override
     public ArrayList<DriverDTO> getAllDriver() {
-        return null;
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<Driver>>() {
+        }.getType());
     }
-}
+    }
+
