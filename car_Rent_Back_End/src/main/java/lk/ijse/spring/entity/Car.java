4b55d584c97fb488c4546ec05d4dadb0ae1637d0
jayani018 <jayaniarunika11@gmail.com>
2023-10-29 14:27:30 +0530

    Created Car Class

---
+++ car_Rent_Back_End/src/main/java/lk/ijse/spring/entity/Car.java
@@ -0,0 +1,37 @@
+package lk.ijse.spring.entity;
+
+import javafx.scene.image.Image;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author : Jayani_Arunika  10/29/2023 - 2:22 PM
+ * @since : v0.01.0
+ **/
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Car {
+    @Id
+    private String carId;
+    private String name;
+    private String brand;
+    private String type;
+    private String image;
+    private int number_Of_Passengers;
+    private String transmission_Type;
+    private String fuel_Type;
+    private String rent_Duration_Price;
+    private String price_Extra_KM;
+    private  String registration_Number;
+    private double free_Mileage;
+    private String color;
+    private String vehicleAvailabilityType;
+}
