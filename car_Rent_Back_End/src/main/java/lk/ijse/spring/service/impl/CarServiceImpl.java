a8891575e445c5ed84739b45b542a89f597ab787
jayani018 <jayaniarunika11@gmail.com>
2023-10-29 16:58:08 +0530

    Updated CarServiceImpl

--- car_Rent_Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
+++ car_Rent_Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
@@ -2,9 +2,11 @@ package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CarDTO;
 import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.entity.Car;
 import lk.ijse.spring.repo.CarRepo;
 import lk.ijse.spring.service.CarService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,21 +27,32 @@ public class CarServiceImpl implements CarService {
     private ModelMapper mapper;
     @Override
     public void saveCar(CarDTO dto) {
-
+        if (repo.existsById(dto.getCarId())) {
+            throw new RuntimeException("Car Already Exist. Please enter another id..!");
+        }
+        repo.save(mapper.map(dto, Car.class));
     }
+
 
     @Override
     public void updateCar(CarDTO dto) {
-
+        if (!repo.existsById(dto.getCarId())) {
+            throw new RuntimeException("Car Not Exist. Please enter Valid id..!");
+        }
+        repo.save(mapper.map(dto, Car.class));
     }
 
     @Override
     public void deleteCar(String id) {
-
+        if (!repo.existsById(id)) {
+            throw new RuntimeException("Wrong ID..Please enter valid id..!");
+        }
+        repo.deleteById(id);
     }
 
     @Override
     public ArrayList<CarDTO> getAllCar() {
-        return null;
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<Car>>() {
+        }.getType());
     }
 }
