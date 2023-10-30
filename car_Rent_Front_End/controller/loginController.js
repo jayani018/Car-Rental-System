027a2fe6d924cb0bf1587255d7cd8230830c23f1
jayani018 <jayaniarunika11@gmail.com>
2023-10-31 01:26:00 +0530

    Updated Login Page Front End

--- car_Rent_Front_End/controller/loginController.js
+++ car_Rent_Front_End/controller/loginController.js
@@ -6,3 +6,49 @@ showPopupBtn.addEventListener("click", () => {
 });
 
 hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+let  baseUrlLogin = "http://localhost:8081/car_Rent_Back_End_war/";
+
+$("#btnLogin").on("click",function () {
+    login;
+});
+
+function login() {
+    let loginRole = $("").val();
+    let loginUserName = $("#userName").val();
+    let loginPassword = $("#password").val();
+
+    $.ajax({
+        url: baseUrlLogin + "LoginForm",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (res) {
+            for (var login of res.data) {
+                if (loginRole_Type === login.role_Type && loginUserName === login.userName && loginPassword === login.password) {
+                    if (loginRole_Type === "DRIVER" && loginUserName === login.userName && loginPassword === login.password) {
+                        $.ajax({
+                            url: baseUrlLogin + "LoginForm/?username=" + loginUserName + "&password=" + loginPassword,
+                            data: res.data,
+                            method: "get",
+                            success: function (res1) {
+                            }
+                        })
+                        window.location.href = 'driverDashboard.html';
+                    } else if (loginRole_Type === "REGISTERED_USER" && loginUserName === login.user_Name && loginPassword === login.password) {
+                        $.ajax({
+                            url: baseUrlLogin + "loginForm/?username=" + loginUserName + "&password=" + loginPassword,
+                            data: res.data,
+                            method: "get",
+                            success: function (res1) {
+                            }
+                        });
+                        window.location.href = 'reg_UserDashboard.html';
+                    } else if (loginRole_Type === "ADMIN" && loginUserName === login.user_Name && loginPassword === login.password) {
+                        window.location.href = 'adminDashboard.html';
+                    }
+                    return;
+                }
+            }
+        }
+    });
+}
