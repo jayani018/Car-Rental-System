ac7381cfc0ec4e1ed53b297178587d08ae146223
jayani018 <jayaniarunika11@gmail.com>
2023-11-02 01:29:11 +0530

    Updated

--- car_Rent_Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
+++ car_Rent_Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -28,7 +28,7 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public void saveAdmin(AdminDTO dto) {
-        if (repo.existsById(dto.getAdminId())) {
+        if (repo.existsById(dto.getUserID())) {
             throw new RuntimeException("Admin Already Exist. Please enter another id..");
         }
         repo.save(mapper.map(dto, Admin.class));
@@ -37,7 +37,7 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public void updateAdmin(AdminDTO dto) {
-        if (!repo.existsById(dto.getAdminId())) {
+        if (!repo.existsById(dto.getUserID())) {
             throw new RuntimeException("Admin Not Exist. Please enter Valid id..!");
         }
         repo.save(mapper.map(dto, Admin.class));
