4b219cdaa79895dc5cfd8303e67a91c8aeaf7e0d
jayani018 <jayaniarunika11@gmail.com>
2023-11-03 17:22:21 +0530

    Updated

---
+++ car_Rent_Back_End/src/main/java/lk/ijse/spring/controller/RentController.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Jayani_Arunika  11/3/2023 - 5:21 PM
+ * @since : v0.01.0
+ **/
+@RestController
+@CrossOrigin
+@RequestMapping("/rent")
+public class RentController {
+}
